8eb42f010aa299cde74f09b879738812ce6ed3d1
Carlos Almeida <carlosalmeida@macbooksalmeida.home>
2017-07-12 16:36:30 +0100

    Initial Commit

---
+++ SystemK/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SystemK
+//
+//  Created by Carlos Almeida on 12/07/17.
+//  Copyright © 2017 weHD. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
